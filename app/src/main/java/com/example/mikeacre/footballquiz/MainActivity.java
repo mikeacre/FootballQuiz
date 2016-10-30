3fa0948645e40e41b5e0dce47304ba0396648c93
mikeacre <mikeacre@gmail.com>
2016-10-30 19:52:44 -0400

    comments fixed
    version 2

--- app/src/main/java/com/example/mikeacre/footballquiz/MainActivity.java
+++ app/src/main/java/com/example/mikeacre/footballquiz/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.mikeacre.footballquiz;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,16 +18,23 @@ import android.widget.Toast;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+
+
 public class MainActivity extends AppCompatActivity {
 
     int questionsLeft = 4;
     int totalQuestions = questionsLeft;
     int currQuestion = 0;
-    int correctAnswers =0;
     String[] allQuestions;
     int[] allCorrect = new int[questionsLeft];
     int[] storeIds = new int[questionsLeft];
     private RadioGroup aRadio;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,11 +50,13 @@ public class MainActivity extends AppCompatActivity {
                 finished();
             }
         });
+
     }
 
-    private void finished(){
+    private void finished() {
         int currCheck = 0;
         int totalCorrect = 0;
+        String toastMessage;
         final EditText textBox = (EditText) findViewById(R.id.textAnswer);
         String textAnswer = textBox.getText().toString();
         final CheckBox cb1 = (CheckBox) findViewById(R.id.cb1);
@@ -55,72 +66,82 @@ public class MainActivity extends AppCompatActivity {
         final CheckBox cb5 = (CheckBox) findViewById(R.id.cb5);
         final CheckBox cb6 = (CheckBox) findViewById(R.id.cb6);
 
-        if(cb2.isChecked()&&cb3.isChecked()&&cb5.isChecked())
-            if(!(cb1.isChecked()||cb4.isChecked()||cb6.isChecked()))
+        if (cb2.isChecked() && cb3.isChecked() && cb5.isChecked())
+            if (!(cb1.isChecked() || cb4.isChecked() || cb6.isChecked()))
                 totalCorrect++;
 
-        if(textAnswer.equals("52"))
-            totalCorrect=totalCorrect+1;
+        if (textAnswer.equals("52"))
+            totalCorrect = totalCorrect + 1;
 
-        while(currCheck < totalQuestions) {
+        while (currCheck < totalQuestions) {
             RadioGroup radioButtonGroup = (RadioGroup) findViewById(storeIds[currCheck]);
             int radioButtonID = radioButtonGroup.getCheckedRadioButtonId();
             if (radioButtonID == allCorrect[currCheck])
                 totalCorrect = totalCorrect + 1;
-            currCheck = currCheck+1;
+            currCheck = currCheck + 1;
         }
-        Toast.makeText(getBaseContext(), "You got "+totalCorrect+" answers correct", Toast.LENGTH_SHORT).show();
+
+        if(totalCorrect >= totalQuestions)
+            toastMessage = "100% correct, Great Job!!";
+        else if(totalCorrect > (totalCorrect * 0.5))
+            toastMessage = "Well you passed, but only "+totalCorrect+" correct.";
+        else
+            toastMessage = "Sorry, you did not pass only "+totalCorrect+" correct";
+
+
+
+        Toast.makeText(getBaseContext(), toastMessage, Toast.LENGTH_SHORT).show();
     }
 
-    private void loadQuestions(){
-        while(questionsLeft > 0){
+    private void loadQuestions() {
+        while (questionsLeft > 0) {
             writeQuestion();
         }
     }
 
-    private void addAnswer(String content,int correct)
-    {
-        View main2 = findViewById(R.id.main);
+    private void addAnswer(String content, int correct) {
         RadioButton aOne = new RadioButton(this);
         aOne.setText(content);
-        aOne.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.WRAP_CONTENT));
+        aOne.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
         ((LinearLayout) aRadio).addView(aOne);
 
-        if(correct == 1 )
+        if (correct == 1)
             allCorrect[currQuestion] = aOne.getId();
     }
 
-    private void writeQuestion(){
+    private void writeQuestion() {
         String[] thisQuestion = allQuestions[currQuestion].split("@@");
 
         int answers = 3;
-        int currAnswer=1;
+        int currAnswer = 1;
 
         View main = findViewById(R.id.main);
         TextView qTitle = new TextView(this);
         qTitle.setText(thisQuestion[0]);
-        qTitle.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.WRAP_CONTENT));
+        qTitle.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
         ((LinearLayout) main).addView(qTitle);
 
         aRadio = new RadioGroup(this);
-        aRadio.setId(currQuestion+999);
-        int thisId= aRadio.getId();
+        aRadio.setId(currQuestion + 999);
+        int thisId = aRadio.getId();
         storeIds[currQuestion] = thisId;
-        aRadio.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.WRAP_CONTENT));
+        aRadio.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
         ((LinearLayout) main).addView(aRadio);
 
-        while(answers > 0) {
+        while (answers > 0) {
 
             int correct = 0;
             int thisQuestionInt = Integer.parseInt(thisQuestion[4]);
-            if(thisQuestionInt == currAnswer)
-                correct=1;
+            if (thisQuestionInt == currAnswer)
+                correct = 1;
             addAnswer(thisQuestion[currAnswer], correct);
-            answers = answers-1;
-            currAnswer = currAnswer+1;
+            answers = answers - 1;
+            currAnswer = currAnswer + 1;
         }
 
-        questionsLeft = questionsLeft-1;
-        currQuestion = currQuestion+1;
+        questionsLeft = questionsLeft - 1;
+        currQuestion = currQuestion + 1;
     }
+
+
 }
